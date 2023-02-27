ebf9d0bc69b4f70e46d12e9a2f802d9dec00e57b
Felix Mendez <mendez.felixrn@gmail.com>
2023-02-26 23:40:45 -0500

    create book class

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ with open("books.csv", "r", newline="\n") as read_list_of_books:
         csv_list.append(row)
     read_list_of_books.close()
     
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.burrowed = False
+    
+    def burrow_book(self, book):
+        if not book.borrowed:
+            book.borrowed = True
+            print(f"Success! {book.title} by {book.author} has been burrowed.")
+        else:
+            print(f"{book.title} by {book.author} has already been burrowed.")
+
+    def return_book(self, book):
+        if book.burrowed:
+            book.burrowed = False
+            print(f"Success! {book.title} by {book.author} has been returned.")
+        else:
+            print(f"{book.title} by {book.author} has not been burrowed.")
